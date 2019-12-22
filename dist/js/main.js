eaa722c54110bb58b0883b6746d1d4bafd2bea38
Goran Ferbišek <goran.ferbisek@gmail.com>
2019-12-22 22:19:13 +0100

    Redesign header section

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".menu-button");
-const nav = document.querySelector(".navbar-links");
+const nav = document.querySelector(".nav-links");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");

b3b358f5d144e57c6497f120ca44bba6a2168725
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-28 17:51:22 +0100

    Add a scroll top button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav-links");
+const topButton = document.querySelector(".top-button");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
@@ -8,3 +9,13 @@ navToggle.addEventListener("click", () => {
 nav.addEventListener("click", () => {
   document.body.classList.remove("nav-open");
 });
+
+topButton.addEventListener('click', () => {
+  console.log(document.documentElement.scrollTop);
+
+  document.documentElement.scrollTop = 0;
+});
+
+function topButtonVisibility() {
+
+}

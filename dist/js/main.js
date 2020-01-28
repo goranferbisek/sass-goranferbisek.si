9bc8526e2215d94ef0f5b36f4138e1074a603a27
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-01-28 19:17:01 +0100

    Add top button style and functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,8 @@ const navToggle = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav-links");
 const topButton = document.querySelector(".top-button");
 
+
+/* navigation functions */
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
@@ -10,12 +12,16 @@ nav.addEventListener("click", () => {
   document.body.classList.remove("nav-open");
 });
 
-topButton.addEventListener('click', () => {
-  console.log(document.documentElement.scrollTop);
 
+/* top button functions */
+topButton.addEventListener('click', () => {
   document.documentElement.scrollTop = 0;
 });
 
-function topButtonVisibility() {
-
+window.onscroll = () => {
+  if (document.documentElement.scrollTop < window.innerHeight) {
+    topButton.style.display = 'none';
+  } else {
+    topButton.style.display = 'block';
+  }
 }
